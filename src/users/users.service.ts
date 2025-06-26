2e586249942b3f3823242f38532397c326ebee13
andrygasy <36558503+andrygasy@users.noreply.github.com>
2025-06-26 13:34:07 +0100

    Initialize NestJS project with auth and docker

---
+++ src/users/users.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(dto: CreateUserDto): Promise<User> {
+    const user = this.usersRepository.create({
+      email: dto.email,
+      password: await bcrypt.hash(dto.password, 10),
+    });
+    return this.usersRepository.save(user);
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+}
